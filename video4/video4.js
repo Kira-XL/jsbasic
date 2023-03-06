e64598c3c44f5fedf0d79b9994aac904189044b0
Kira-XL <hoangnt7492@gmail.com>
2023-03-06 12:20:13 +0700

    video 4

---
+++ video4/video4.js
@@ -0,0 +1,12 @@
+let a = 'Tôi là string/chuỗi';
+let b = "abcxyz123 "
+let c = `We are the so-called "Vikings" 
+from the north.`;
+
+console.log(a);
+
+console.log(4 + 5 + 'a');
+console.log('a' + 4 + 5);
+console.log(b.length);
+console.log(b.trim().length);
+console.log(c);
