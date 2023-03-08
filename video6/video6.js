fbf3e1f22717889f2ee730e5eceb2d0d69686ece
Kira-XL <hoangnt7492@gmail.com>
2023-03-08 11:08:44 +0700

    video 6

---
+++ video6/video6.js
@@ -0,0 +1,18 @@
+console.log('Hello world from js'.toUpperCase());
+
+let a = 5;
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = +d; // hoac dung let e = Number(d);
+
+console.log(a+b);
+console.log(a+c);
+console.log(a+d);
+console.log(a-d);
+console.log(a*d);
+console.log(a/d);
+console.log(a === d)
+console.log(a==d);
+console.log('a:', typeof a, 'd:', typeof d);
+console.log('e value:', e, 'type:', typeof e );
