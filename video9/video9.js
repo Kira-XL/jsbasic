d497f5a0efa4d396a88feb34f1498d51e1f56d0e
Kira-XL <hoangnt7492@gmail.com>
2023-03-10 10:41:15 +0700

    video9 Array

---
+++ video9/video9.js
@@ -0,0 +1,22 @@
+let a = {
+    name : 'Kevin',
+    age: 25
+};
+
+console.log(`check data obj: ${a.name} , ${a.age}`, a);
+
+let b = ['MU', 29, 'SUper'];
+
+console.log(`type of b: ${typeof b}, ${b}`);
+console.log('type of b: ', typeof b, b);
+
+console.log('first item:', b[0], 'second item:', b[1]);
+console.log(`first item: ${b[0]}, second item: ${b[1]}`);
+
+let family = [
+    {name: 'John', age: 59},
+    {name: 'Jenn', age: 57}
+];
+
+console.log('member of the family:', family);
+console.log(`member of the family:${family[0]}`);
