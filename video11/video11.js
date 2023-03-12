883a0dff618d85d14985f07e1f709fb3c9a14003
Kira-XL <hoangnt7492@gmail.com>
2023-03-12 11:57:14 +0700

    video11 loop

---
+++ video11/video11.js
@@ -0,0 +1,9 @@
+let arr = ['Kevin','Thomas','Mark','Jenny'];
+
+let i = 0;
+
+for (let i=0; i < arr.length; i++){
+    console.log('check variable i = ', arr[i]);
+}
+
+console.log('check value of i:', i);
