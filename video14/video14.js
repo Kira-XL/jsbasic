06e2868b630ac0d426af7d8829dc6c93b6ddae8e
Kira-XL <hoangnt7492@gmail.com>
2023-03-16 12:15:38 +0700

    video 14 switch case/ case in case

---
+++ video14/video14.js
@@ -0,0 +1,25 @@
+let day ='';
+let a = 0;
+
+switch (a) {
+    case 0:     
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+
+  console.log('day:', day, new Date().getDay());
